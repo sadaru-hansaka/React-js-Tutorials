1b3063bfa87131cfaa0fb455a27e57ad333e7d9b
Sadaru Hansaka <sandaruhansaka0828@gmail.com>
2024-08-27 07:28:49 +0530

    Learned how to use Tailwind css with react

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,16 @@
+import {React, useState } from 'react'
+
+function App() {
+  return (
+    <div className='w-full'>
+      <section className='w-full m-12 flex flex-col items-center justify-center text-center text-3xl h-screen bg-twitter-blue-400 sm: font-bold'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sint dolor? Placeat perspiciatis officiis eaque ullam rem veritatis. Nam similique qui recusandae distinctio laborum quas aliquam, labore unde. Aperiam, fuga.</p>
+        <div className='card'>
+          This is a card
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind-css/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    
+    extend: {
+      colors:{
+        "twitter-blue" : "#1DA1F2",
+      },
+    },
+  },
+  plugins: [],
+}
+
